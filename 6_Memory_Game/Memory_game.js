9fd923fa4b550a580c0ce2e234ee9d5fcb164e17
rishabhacejain <153978390+rishabhacejain@users.noreply.github.com>
2025-02-20 11:32:28 +0530

    Add files via upload
    logic upadte

--- 6_Memory_Game/Memory_game.js
+++ 6_Memory_Game/Memory_game.js
@@ -13,7 +13,7 @@ let shuffledCards = [];
 let flippedCards = [];
 let matches = 0;
 let startTime;
-let timerID; 
+let timerID;
 let bestTime = localStorage.getItem("bestTime");
 
 if (bestTime) {
@@ -26,60 +26,60 @@ if (bestTime) {
 
 
 imageUpload.addEventListener("change", function (e) {
-    
+
     userImages = [];
     const files = e.target.files;
-    
-    if (files.length < 2) {
-        alert("Upload atleast 2 images");
-        
-        
+
+    if (files.length < 6) {
+        alert("Upload atleast 6 images");
+
+
         return;
-        
+
     } else if (files.length > 16 || files.length % 2 !== 0) {
-        
+
         alert("Upload In even Number like 6,8,10,12,14 or Max 16 ");
         return;
-        
+
     }
-    
-    
-    
+
+
+
     for (let file of files) {
-        
+
         const imgURL = URL.createObjectURL(file);
         userImages.push(imgURL);
         console.log(imgURL);
-        
+
     }
-    
+
 });
 
 
 //start game
 
 startGameBtn.addEventListener("click", () => {
-    
-    
-    if (userImages.length < 2) {
+
+
+    if (userImages.length < 5) {
         alert("Please Upload Atleast 5 Images");
         return;
     }
     const clock = document.getElementById("clock");
-    
-    
-    
-    
+
+
+
+
     msg.style.display = 'none'; // Hide previous messages
     clearInterval(timerID); // Clear any existing timer
     timer.innerHTML = "00:00"; // Reset Timer UI
-    
+
     startTime = Date.now();
     timerID = setInterval(updateTimer, 1000);
     timer.style.display = 'block'
 
     setupGame();
-    
+
 });
 
 function updateTimer() {
@@ -97,35 +97,35 @@ function setupGame() {
     flippedCards = [];
 
     const totalCards = shuffledCards.length;
-    
-     if((totalCards % 4 == 0) && (totalCards <= 12)){
-         
-         gameBoard.style.gridTemplateColumns = `repeat(4, 1fr)`;
-        }else if(totalCards % 5 ==0){
-         gameBoard.style.gridTemplateColumns = `repeat(5, 1fr)`;
-     } else {
-         gameBoard.style.gridTemplateColumns = `repeat(8, 1fr)`;
-     }
+
+    if ((totalCards % 4 == 0) && (totalCards <= 12)) {
+
+        gameBoard.style.gridTemplateColumns = `repeat(4, 1fr)`;
+    } else if (totalCards % 5 == 0) {
+        gameBoard.style.gridTemplateColumns = `repeat(5, 1fr)`;
+    } else {
+        gameBoard.style.gridTemplateColumns = `repeat(8, 1fr)`;
+    }
     shuffledCards.forEach((image, index) => {
         const card = document.createElement("div");
         card.classList.add("card");
         card.dataset.index = index;
-        
+
         const img = document.createElement("img");
         img.src = image;
         card.appendChild(img);
-        
+
         card.addEventListener("click", () => flipCard(card));
         gameBoard.appendChild(card);
-        
-        
+
+
     });
-    
+
 }
 //shulffle card
 
 function shuffleCards(cards) {
-    
+
     return cards.sort(() => Math.random() - 0.5)
 }
 
@@ -133,14 +133,14 @@ function shuffleCards(cards) {
 
 function flipCard(card) {
     if (flippedCards.length < 2 && !card.classList.contains("is-flipped")) {
-        
+
         card.classList.add("is-flipped");
         card.querySelector("img").style.display = "block";
         flippedCards.push(card)
     }
-    
+
     if (flippedCards.length === 2) {
-        
+
         setTimeout(checkMatch, 800)
     }
 }
@@ -148,9 +148,9 @@ function checkMatch() {
     const [card1, card2] = flippedCards;
     const img1 = card1.querySelector("img").src;
     const img2 = card2.querySelector("img").src;
-    
+
     if (img1 === img2) {
-        
+
         card1.classList.add("is-match");
         card2.classList.add("is-match");
         matches += 2;
@@ -161,32 +161,32 @@ function checkMatch() {
         card1.querySelector("img").style.display = "none";
         card2.querySelector("img").style.display = "none";
     }
-    
+
     flippedCards = [];
-    
+
     if (matches === shuffledCards.length) {
         clearInterval(timerID); // Clear any existing timer
         const endTime = Date.now();
         const totalTime = ((endTime - startTime) / 1000).toFixed(2);
-       
-        bestTime = bestTime ? parseFloat(bestTime) : Infinity; 
-        console.log(typeof(totalTime));
-        console.log(typeof(bestTime));
-        if(totalTime < bestTime){
-            
+
+        bestTime = bestTime ? parseFloat(bestTime) : Infinity;
+        console.log(typeof (totalTime));
+        console.log(typeof (bestTime));
+        if (totalTime < bestTime) {
+
             bestTime = totalTime;
             localStorage.setItem("bestTime", bestTime);
             displayBestTime.innerHTML = `Best Time: ${bestTime} seconds`
         }
-        
-        
+
+
         setTimeout(() => {
-            
+
             confetti();
             msg.innerHTML = `Game finished! You took 
             ${totalTime} seconds!`;
             msg.style.display = 'block';
-    
+
         }, 500);
 
     }
